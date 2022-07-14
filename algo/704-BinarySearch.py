487e8b97885ea6f7dc46b27549cb668e9cf130bd
rjun <rjun0729@gmail.com>
2022-07-14 13:07:45 +0800

    update 704-BinarySearcsh

--- algo/704-BinarySearch.py
+++ algo/704-BinarySearch.py
@@ -8,7 +8,7 @@ class Solution:
 
         while left <= right:
           mid = (left + right) / 2
-          if nums[mid] = target:
+          if nums[mid] == target:
             return mid
           elif nums[mid] > target:
             right = mid - 1
