fbd2984e61e67b9b4e6d85090b03a7cab1d06b7f
rjun <rjun0729@gmail.com>
2022-07-14 14:55:01 +0800

    modify 1-TwoSum

--- ds-1/1-TwoSum.py
+++ ds-1/1-TwoSum.py
@@ -4,18 +4,13 @@
 #     2. 查找 number 是否在 Hash Table 的 keys 中即為答案
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        returnResult = [None] * 2
         hashTable = {}
 
-        for i, number in enumerate(nums):
-          if number in hashTable.keys():
-            returnResult[0] = hashTable[number]
-            returnResult[1] = i
+        for i, n in enumerate(nums):
+          if n in hashTable.keys():
+            return [hashTable[n], i]
 
-          if number not in hashTable.keys():
-            hashTable[target - number] = i
-          
-
-        return returnResult
+          #  n not in hashTable.keys():
+          hashTable[target - n] = i
 
   
