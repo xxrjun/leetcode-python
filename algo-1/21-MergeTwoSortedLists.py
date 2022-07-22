86915d969dd49521b81c1311fc61fb3721fe1f89
rjun <rjun0729@gmail.com>
2022-07-23 03:11:35 +0800

    Finished Algorithm I Day 10

---
+++ algo-1/21-MergeTwoSortedLists.py
@@ -0,0 +1,9 @@
+# 21. Merge Two Sorted Lists
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
