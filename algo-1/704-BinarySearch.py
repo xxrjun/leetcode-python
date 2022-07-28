6a4e6c25d1605923e1067d06f31170e8251a0eba
xxrjun <rjun0729@gmail.com>
2022-07-28 10:13:41 +0800

    update Algorithm I Day1.

--- algo-1/704-BinarySearch.py
+++ algo-1/704-BinarySearch.py
@@ -2,15 +2,18 @@
 # Idea:
 #     1. 有左右，每次找中間並更新左或右
 class Solution:
+
     def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
 
         while left <= right:
-          mid = (left + right) / 2
-          if nums[mid] == target:
-            return mid
-          elif nums[mid] > target:
-            right = mid - 1
-          else:
-            left = mid + 1
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
