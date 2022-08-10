ce75c99a073eaa6dea96a3059e5d08774ca86248
rjun <rjun0729@gmail.com>
2022-08-10 12:11:11 +0800

    Adjust files.

--- again/235-LowestCommonAncestorofaBinarySearchTree.py
+++ again/235-LowestCommonAncestorofaBinarySearchTree.py
@@ -11,12 +11,3 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        cur = root
-
-        while cur:
-            if cur.val > p.val and cur.val > q.val:
-                cur = cur.left
-            elif cur.val < p.val and cur.val < q.val:
-                cur = cur.right
-            else:
-                return cur
