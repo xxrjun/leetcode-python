88fe145886ae2373846c77904f7d10e88e54f671
xxrjun <rjun0729@gmail.com>
2022-08-11 10:30:52 +0800

    Finished Binary Search I Day 1.

---
+++ binarysearch-1/704-BinarySearch.py
@@ -0,0 +1,20 @@
+# 704. Binary Search
+
+# 704. Binary Search
+# Idea:
+#     1. 有左右，每次找中間並更新左或右
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
