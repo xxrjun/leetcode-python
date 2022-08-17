5ffb244c7f3efd98ec8e4d4e5efe2c72dd1e24ac
rjun <rjun0729@gmail.com>
2022-08-17 12:08:34 +0800

    Finished Binary Search I Day 6.

---
+++ binarysearch-1/1539-KthMissingPositiveNumber.py
@@ -0,0 +1,28 @@
+# 1539. Kth Missing Positive Number
+
+class Solution:
+    # Solution 1: Cool
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        # 小於 K 的算 not missing nubmer
+        # Not missing number 會讓 k 增加，因為我們要算的是 Kth missing number
+        for n in arr:
+            if n <= k:
+                k += 1
+            else:
+                break
+
+        return k
+
+    # Solution 2: Binary Search (Faster)
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        left, right = 0, len(arr) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if arr[mid] - mid - 1 < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left + k
