964eaa586d921acaabdcd18924a9da2c8e123804
rjun <rjun0729@gmail.com>
2022-08-17 12:10:07 +0800

    Add 1539. Kth Missing Positive Number and 441. Arraying Conins to Doing Again.

---
+++ again/1539-KthMissingPositiveNumber.py
@@ -0,0 +1,5 @@
+# 1539. Kth Missing Positive Number
+
+class Solution:
+    # Solution 1: Cool
+    def findKthPositive(self, arr: List[int], k: int) -> int:
---
+++ again/441-ArrangingCoins.py
@@ -0,0 +1,9 @@
+# 441. Arranging Coins
+
+class Solution:
+    # Solution 1: Brute Force
+    def arrangeCoins(self, n: int) -> int:
+
+        # Solution 2: Binary Search
+
+    def arrangeCoins(self, n: int) -> int:
