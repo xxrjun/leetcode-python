097176cacb3010e9df63e39a33745c9a61c0cb57
rjun <rjun0729@gmail.com>
2022-08-17 13:17:45 +0800

    Update 1539. Kth Missing Positive Number

--- binarysearch-1/1539-KthMissingPositiveNumber.py
+++ binarysearch-1/1539-KthMissingPositiveNumber.py
@@ -20,7 +20,7 @@ class Solution:
         while left <= right:
             mid = left + (right - left) // 2
 
-            if arr[mid] - mid - 1 < k:
+            if arr[mid] - (mid + 1) < k:
                 left = mid + 1
             else:
                 right = mid - 1
