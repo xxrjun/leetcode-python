2aef45b195dd38bbfcafe0939a5fb4063afa97d8
rjun <rjun0729@gmail.com>
2022-08-22 12:17:38 +0800

    Move ds-1, algo-1 and binarysearch-1 to study-plan folder.

--- algo-1/136-SingleNumber.py
+++
@@ -1,9 +0,0 @@
-# 136. Single Number
-
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        n = 0
-        for i in range(len(nums)):
-            n = n ^ nums[i]
-
-        return n
--- binarysearch-1/1337-TheKWeakestRowsinaMatrix.py
+++
@@ -1,6 +0,0 @@
-# 1337. The K Weakest Rows in a Matrix
-
-class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        # Get the sum of each row and sort
-        return sorted(range(len(mat)), key=lambda row: sum(mat[row]))[:k]
