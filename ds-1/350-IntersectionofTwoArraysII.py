f7257351844b5d1d647870acc1a4253866ae148f
rjun <rjun0729@gmail.com>
2022-08-22 12:13:00 +0800

    Update 350. in ds-1.

--- ds-1/350-IntersectionofTwoArraysII.py
+++ ds-1/350-IntersectionofTwoArraysII.py
@@ -1,39 +1,59 @@
 # 350. Intersection of Two Arrays II
-# Idea:
-#     1. 把第一個 array 依 (number, times) 放入 hash table
-#     2. 另一個 list 去比較 hash table 並且 update times，有找到的話放入 returnResult
+
 
 class Solution:
-		def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-			returnResult = []
-			hashTable = {}	# (number, times)
+    # Solution 1: Dictionary
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-			hashTable = self.getHashTable(nums1)
-			returnResult = self.findInterSection(nums2, hashTable)
+        def getDict(nums: List[int]) -> Dict:
+            dict = {}  # (number, counts)
+            for n in nums:
+                if n not in dict:
+                    dict[n] = 1
+                else:
+                    dict[n] += 1
 
-			return returnResult
-		
+            return dict
 
-		# Helper funciton to get hash table from a List
-		def getHashTable(self, numList: List[int]):
-				returnHashTable = {}
-				
-				for n in numList:
-					if n not in returnHashTable.keys():
-						returnHashTable[n] = 1
-					elif n in returnHashTable.keys():
-						returnHashTable[n] += 1
+        def findIntersection(dict: Dict, nums: List[int]) -> List[int]:
+            intersection = []
 
-				return returnHashTable
+            for n in nums:
+                if n in dict.keys() and dict[n] > 0:
+                    intersection.append(n)
+                    dict[n] -= 1
+
+            return intersection
+
+        dict = getDict(nums1)
+
+        return findIntersection(dict, nums2)
+
+    # Solution 2: Two Pointers
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        intersection = []
+
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        p1, p2 = 0, 0
+
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] == nums2[p2]:
+                intersection.append(nums1[p1])
+                p1 += 1
+                p2 += 1
+            elif nums1[p1] > nums2[p2]:
+                p2 += 1
+            else:
+                p1 += 1
+
+        return intersection
 
 
-		# Helper function to find intersection from a list and hashTable
-		def findInterSection(self, numList: List[int], hashTable: Dict):
-				returnResult = []
-
-				for n in numList:
-					if n in hashTable.keys() and hashTable[n] > 0:
-						returnResult.append(n)
-						hashTable[n] -= 1
-
-				return returnResult
+"""
+    Follow up:
+        1. What if the given array is already sorted? How would you optimize your algorithm?
+        2. What if nums1's size is small compared to nums2's size? Which algorithm is better?
+        3. What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load 
+              all elements into the memory at once?
+"""
